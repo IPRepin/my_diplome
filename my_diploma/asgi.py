893f361ade3240130218074d950ae650fdf7923c
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2023-07-04 09:17:58 +0300

    rename

--- my_diploma/asgi.py
+++ my_diploma/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for my_diplome project.
+ASGI config for my_diploma project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_diplome.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_diploma.settings')
 
 application = get_asgi_application()
--- my_diploma/wsgi.py
+++ my_diploma/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for my_diplome project.
+WSGI config for my_diploma project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_diplome.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_diploma.settings')
 
 application = get_wsgi_application()
